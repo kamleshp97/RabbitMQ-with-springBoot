15e3e2210c667a2e6e5202914eb1b70e180d5bf9
kamleshp97 <kamleshp097@gmail.com>
2023-11-23 20:58:41 +0530

    added json message config

--- springboot-rabbitmq/src/main/java/com/springbootrabbitmq/config/RabbitMqConfig.java
+++ springboot-rabbitmq/src/main/java/com/springbootrabbitmq/config/RabbitMqConfig.java
@@ -1,9 +1,14 @@
 package com.springbootrabbitmq.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +23,12 @@ public class RabbitMqConfig {
 	
 	@Value("${rabbitmq.routingkey.name1}")
 	private String routingkey;
+	
+	@Value("${rabbitmq.queue.jsonname1}")
+	private String jsonQueue;
+	
+	@Value("${rabbitmq.routingkey.jsonname1}")
+	private String jsonRoutingkey;
 
 
 	@Bean
@@ -40,4 +51,31 @@ public class RabbitMqConfig {
 	}
 	
 	//connectionFactory, RabbitTemplate and RabbitAdmin beans are auto-configured by spring boot.
+	
+	//handling json message
+	@Bean
+	public Queue jsonQueue() {
+		return new Queue(jsonQueue);
+	}
+	
+	@Bean
+	public Binding jsonBinding() {
+		return BindingBuilder
+				.bind(jsonQueue())
+				.to(exchange1())
+				.with(jsonRoutingkey);			
+	}
+	
+	//required converter for json message
+	@Bean
+	public MessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	
+	@Bean
+	public AmqpTemplate template(ConnectionFactory factory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
+		rabbitTemplate.setMessageConverter(converter());
+		return rabbitTemplate;
+	}
 }
